0d19789eb88ba986ee32cf60aeba03a74539c5ef
Kraksi <krasnov.d.a.20@mail.ru>
2022-11-14 13:57:56 +0300

    Added the output of several responses with the selection of the most similar

--- Livenshtein.py
+++ Livenshtein.py
@@ -7,24 +7,65 @@ funcs = [fuzz.ratio, fuzz.token_sort_ratio, fuzz.partial_ratio, fuzz.token_set_r
          fuzz.partial_token_set_ratio, fuzz.partial_token_sort_ratio, fuzz.QRatio, fuzz.WRatio]
 
 
+def sort_questions(list_of_questions):
+    list_of_questions.sort(reverse=True)
+    result_list = []
+    final_list = []
+    number_of_elements = 0
+    for item in list_of_questions:
+        number_of_elements += 1
+    if number_of_elements > 3:
+        for i in range(0, 3):
+            result_list.append(list_of_questions[i])
+    elif number_of_elements == 3:
+        for i in range(0, 2):
+            result_list.append(list_of_questions[i])
+    elif number_of_elements == 2:
+        result_list.append(list_of_questions[0])
+    elif number_of_elements == 1:
+        result_list.append(list_of_questions[0])
+    for ans in result_list:
+        final_list.append(ans[1])
+    return final_list
+
+
 def get_coef(questions, text):
     coef_max = 0
+    coef_oth = 0
+    result_for_list = None
     answer = None
+    list_q = []
     for question in questions:
         coef_cur = 0
         for quest in question:
             for f in funcs:
-                coef_cur += f(quest.replace('u:', ''), text)
-        if coef_cur > coef_max:
+                coef_cur += f(quest, text)
+        if ((coef_cur > coef_max) or (coef_cur > (coef_oth - 300))):
             coef_max = coef_cur
+            coef_oth = coef_max
             answer = question[1]
-    print(coef_max)
-    return answer
+            result_for_list = [coef_max, answer]
+            list_q.append(result_for_list)
+
+    #    return answer
+    return list_q
 
 
 def answer_db(text):
+    answer = []
     text = text.lower().strip()
     mas = sqlite_db.sql_read_1()
-    answer = get_coef(mas, text)
+    answer = sort_questions(get_coef(mas, text))
 
-    return f'Ваш ответ:\n{answer}'
+    #    return f'Ваш ответ:\n{answer}'
+    return answer
+
+
+def pop_answer(l):
+    popular_answer = None
+    num = 0
+    for qu in l:
+        if num == 0:
+            popular_answer = qu
+            num += 1
+    return popular_answer
--- handlers/other.py
+++ handlers/other.py
@@ -3,10 +3,23 @@ from create_bot import dp, bot
 import Livenshtein as l
 from keyboards import kb_client_inline_full
 
-async def echo_send(message: types.Message):
 
+async def echo_send(message: types.Message):
+    number = 0
+    popular_answer = None
     s = l.answer_db(message.text)
-    await bot.send_message(message.from_user.id, s)
+    popular_answer = l.pop_answer(s)
+    await bot.send_message(message.from_user.id, '--------------------\n\nНаиболее подходящий '
+                                                 'ответ:\n\n--------------------')
+    await bot.send_message(message.from_user.id, popular_answer)
+    await bot.send_message(message.from_user.id, '--------------------\n\nДругие похожие ответы:'
+                                                 '\n\n--------------------')
+    for answer in s:
+        number += 1
+        if number == 1:
+            continue
+        await bot.send_message(message.from_user.id, answer)
+
     await bot.send_message(message.from_user.id, '--------------------\n\nВам помог ответ? Если нет, можете '
                                                  'попробовать сформулировать вопрос по другому или подать запрос на '
                                                  'связь со специалистом\n\n--------------------',
